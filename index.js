5d4aa9da65dede74b215c7649dc0da5d53acffd8
wpdlzm <jake01218@naver.com>
2020-10-10 21:26:56 +0900

    add !전체수정

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.argv.length == 2 ? process.env.token : "";
+const welcomeChannelName = "환영합니다";
+const welcomeChannelComment = "스텔라 공식몰에 오신것을 환영합니다!";
 
 client.on('ready', () => {
   console.log('켰다.');
   client.user.setPresence({ game: { name: '모든문의는 KADE#1004' }, status: 'online' })
 });
 
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  const deleteUser = member.user;
+  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+
+  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
+});
 
 client.on('message', (message) => {
   if(message.author.bot) return;
@@ -27,33 +36,44 @@ client.on('message', (message) => {
       .setFooter('Made by KADE', img)
 
     message.channel.send(embed)
-  } else if(message.content == '!원격요청') {
-    let img = 'https://cdn.discordapp.com/attachments/764417429333737482/764453271611113472/Stella.png';
-    let site ='https://www.teamviewer.com/';
+  } else if(message.content == '##$SDSADJCVLKAJLDAD') {
+    let helpImg = 'https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png';
+    let commandList = [
+      {name: '!help', desc: 'help'},
+      {name: 'ping', desc: '현재 핑 상태'},
+      {name: 'embed', desc: 'embed 예제1'},
+      {name: '!전체공지', desc: 'dm으로 전체 공지 보내기'},
+      {name: '!전체공지2', desc: 'dm으로 전체 embed 형식으로 공지 보내기'},
+      {name: '!청소', desc: '텍스트 지움'},
+      {name: '!초대코드', desc: '해당 채널의 초대 코드 표기'},
+      {name: '!초대코드2', desc: '봇이 들어가있는 모든 채널의 초대 코드 표기'},
+    ];
+    let commandStr = '';
     let embed = new Discord.RichEmbed()
-      .setTitle('원격요청')
-      .setThumbnail(img)
-      .setColor('#00dcff')
-      .addBlankField()
-      .addField('팀뷰어를 설치한 후에 관리자나 직원분들에게 문의주세요', site)
+      .setAuthor('BOT', helpImg)
+      .setColor('#186de6')
+      .setFooter(`BOT ❤️`)
       .setTimestamp()
-      .setFooter('Made by KADE', img)
+    
+    commandList.forEach(x => {
+      commandStr += `• \`\`${changeCommandStringLength(`${x.name}`)}\`\` : **${x.desc}**\n`;
+    });
 
     embed.addField('Commands: ', commandStr);
 
     message.channel.send(embed)
-
+  
   } else if(message.content.startsWith('!공지')) {
     if(checkPermission(message)) return
     if(message.member != null) { // 채널에서 공지 쓸 때
-      let contents = message.content.slice('!공지'.length);
+      let contents = message.content.slice('!전체공지2'.length);
       let embed = new Discord.RichEmbed()
-        .setAuthor('스텔라 공지')
+        .setAuthor('공지 of Stella BOT')
         .setColor('#00dcff')
-        .setFooter('Made by KADE')
+        .setFooter(`Stella BOT ❤️`)
         .setTimestamp()
   
-      embed.addField(contents);
+      embed.addField('공지: ', contents);
   
       message.member.guild.members.array().forEach(x => {
         if(x.user.bot) return;
@@ -64,7 +84,19 @@ client.on('message', (message) => {
     } else {
       return message.reply('채널에서 실행해주세요.');
     }
+  } else if(message.content.startsWith('!전체공지')) {
+    if(checkPermission(message)) return
+    if(message.member != null) { // 채널에서 공지 쓸 때
+      let contents = message.content.slice('!전체공지'.length);
+      message.member.guild.members.array().forEach(x => {
+        if(x.user.bot) return;
+        x.user.send(`<@${message.author.id}> ${contents}`);
+      });
   
+      return message.reply('공지를 전송했습니다.');
+    } else {
+      return message.reply('채널에서 실행해주세요.');
+    }
   } else if(message.content.startsWith('!청소')) {
     if(message.channel.type == 'dm') {
       return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
